aa465c101c526dabedb4859d49551649726f6baa
CalliopeVanrogue <harleyquinn771@gmail.com>
2024-12-11 21:47:56 -0800

    Forgot to add game critic script

---
+++ Assets/Scripts/GameCritic.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+{
+// The game was honestly too easy. 
+//It was boring because it was just dodging and weaving. 
+// Phasing through obstacles while having a cooldown made it too easy to avoid obstacles
+
+//I would make the graphics more interesting. Like additional particle effects that appear when you hit a powerup or an obstacle.
+// I would also change up speed. For instance if you hit the powerup you run faster if you hit an obstacle you run slower rather than it just be a timing thing
+
+// The phasing made the game fun but I can honestly say I didn't enjoy anything else about the game
+
+//The whole game was rather tedious for me. Constantly running towards or away from the obstacles was just boring there was nothing really appealing about the game.
+
+
